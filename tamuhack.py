ae6d6de088b4a4876346635fcb6a94edd206028a
Alex B <alexrbellon@gmail.com>
2018-01-27 18:35:00 -0600

    Condensed to one text file, fixed geopy

--- tamuhack.py
+++ tamuhack.py
@@ -10,15 +10,16 @@ access_token_secret = "VYJI7K4QMd65hUg2bfoNoFDSDdG3c43vPjUSZ8Rysox1a"
 consumer_key = "Vlqd8jWbRFEQx3wCLlGvkGz3E"
 consumer_key_secret = "BoTIT6r6QORMCcamSyvzgS9aJ6zlDL8GUaMPqxzUQzPQYC0VMX"
 
-#disasters = ('flood', 'earthquake', 'wildfire', 'tornado', 'hurricane', 'tsunami', 'avalanche')
-disasters = ('love', 'flood')
+disasters = ('flood', 'earthquake', 'wildfire', 'tornado', 'hurricane', 'tsunami', 'avalanche', 'love')
 
 class StdOutListener(StreamListener):
  
     def on_status(self, status):
         tweet = status.text
+        time = str(status.created_at)
 
         if status.coordinates or status.place:
+            print('loc available')
             geolocator = Nominatim()
             if status.coordinates:
                 val = list(status.coordinates.values())
@@ -29,18 +30,21 @@ class StdOutListener(StreamListener):
                 print(loc)
                 coord = ('[' + repr(loc.longitude) + ', ' + repr(loc.latitude) + ']')
 
+            tag = 'IGNORE'
             for dis in disasters:
-                #if dis in tweet:
-                with open("%s.txt" % dis, "a") as myfile:
-                    myfile.write(coord + ',')
+                if dis in tweet:
+                    tag = dis
+
+            with open("output.txt", "a") as myfile:
+                myfile.write(coord + ',' + time + ',' + tag + '\n')
         return True
 
     def on_error(self, status_code):
-        print('Got an error with status code: ' + str(status_code))
+        print('Error: ' + str(status_code))
         return True
  
     def on_timeout(self):
-        print('Timeout...')
+        print('Timeout')
         return True
  
 if __name__ == '__main__':
@@ -48,5 +52,4 @@ if __name__ == '__main__':
     auth = tweepy.OAuthHandler(consumer_key, consumer_key_secret)
     auth.set_access_token(access_token, access_token_secret)
     stream = Stream(auth, listener)
-    #stream.filter(track=['flood', 'earthquake', 'wildfire', 'tornado', 'hurricane', 'tsunami', 'avalanche'])
-    stream.filter(track=['love'])
+    stream.filter(track=['flood', 'earthquake', 'wildfire', 'tornado', 'hurricane', 'tsunami', 'avalanche', 'love'])
